601df1051b56a43b1e2861a2ae2fa71376580eb4
Kostya Shabalev <41301850+KostyaShabalev@users.noreply.github.com>
2019-05-15 23:14:16 +0300

    test commit

--- pet-shop/controllers/petShop.controller.js
+++ pet-shop/controllers/petShop.controller.js
@@ -11,46 +11,46 @@ export class PetShopController {
 	}
 
 	initiatePetShop() {
-		this.fetchAnimalCategories()
-			.then(response => {
+		// this.fetchAnimalCategories()
+		// 	.then(response => {
 
-				return response.json();
-			})
-			.then(data => {
-				const animalCategories = data.categories;
-				const petsToStore = this.transformAnimalsIntoPets(animalCategories);
+		// 		return response.json();
+		// 	})
+		// 	.then(data => {
+		// 		const animalCategories = data.categories;
+		// 		const petsToStore = this.transformAnimalsIntoPets(animalCategories);
 		
-				this.storePets(petsToStore);
-				this.fillTopCategories();
-				this.renderMainElements();
-				this.addMainListeners();
-			});
-		// const animalCategories = this.fetchAnimalCategories();
-		// const petsToStore = this.transformAnimalsIntoPets(animalCategories);
+		// 		this.storePets(petsToStore);
+		// 		this.fillTopCategories();
+		// 		this.renderMainElements();
+		// 		this.addMainListeners();
+		// 	});
+		const animalCategories = this.fetchAnimalCategories();
+		const petsToStore = this.transformAnimalsIntoPets(animalCategories);
 
-		// this.storePets(petsToStore);
-		// this.fillTopCategories();
-		// this.renderMainElements();
-		// this.addMainListeners();
+		this.storePets(petsToStore);
+		this.fillTopCategories();
+		this.renderMainElements();
+		this.addMainListeners();
 	}
 
 	fetchAnimalCategories() {
 		// const url = './assets/pets.json';
 		const url = 'https://kostyashabalev.github.io/pet-shop/assets/pets.json';
-		// let fetchedAnimals;
-		// let animalsRequest = new XMLHttpRequest();
+		let fetchedAnimals;
+		let animalsRequest = new XMLHttpRequest();
 
-		// animalsRequest.open('GET', '../assets/pets.json', false);
+		animalsRequest.open('GET', url, false);
 
-		// animalsRequest.onload = function () {
-		// 	fetchedAnimals = JSON.parse(this.response).categories;
-		// }
+		animalsRequest.onload = function () {
+			fetchedAnimals = JSON.parse(this.response).categories;
+		}
 
-		// animalsRequest.send();
+		animalsRequest.send();
 
-		// return fetchedAnimals;
+		return fetchedAnimals;
 
-		return fetch(url);
+		// return fetch(url);
 	}
 
 	transformAnimalsIntoPets(animalCategories) {
