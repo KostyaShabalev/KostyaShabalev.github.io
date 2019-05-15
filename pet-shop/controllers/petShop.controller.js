59bbd36957f12a0bfa2a8cef2755b0d102de15a2
Kostya Shabalev <41301850+KostyaShabalev@users.noreply.github.com>
2019-05-15 23:00:14 +0300

    another final commit

--- pet-shop/controllers/petShop.controller.js
+++ pet-shop/controllers/petShop.controller.js
@@ -11,6 +11,20 @@ export class PetShopController {
 	}
 
 	initiatePetShop() {
+		// this.fetchAnimalCategories()
+		// 	.then(response => {
+
+		// 		return response.json();
+		// 	})
+		// 	.then(data => {
+		// 		const animalCategories = data;
+		// 		const petsToStore = this.transformAnimalsIntoPets(animalCategories);
+		
+		// 		this.storePets(petsToStore);
+		// 		this.fillTopCategories();
+		// 		this.renderMainElements();
+		// 		this.addMainListeners();
+		// 	});
 		const animalCategories = this.fetchAnimalCategories();
 		const petsToStore = this.transformAnimalsIntoPets(animalCategories);
 
@@ -21,6 +35,7 @@ export class PetShopController {
 	}
 
 	fetchAnimalCategories() {
+		// const url = '../assets/pets.json';
 		let fetchedAnimals;
 		let animalsRequest = new XMLHttpRequest();
 
@@ -33,6 +48,8 @@ export class PetShopController {
 		animalsRequest.send();
 
 		return fetchedAnimals;
+
+		// return fetch(url);
 	}
 
 	transformAnimalsIntoPets(animalCategories) {
